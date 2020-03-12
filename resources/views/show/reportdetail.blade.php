a9afe3fa3a5bcec00edddee70a75064e3cacd2d3
alanichsan <alanxrpl2018smkrus@gmail.com>
2020-03-12 14:14:41 +0700

    report detail

--- resources/views/show/reportdetail.blade.php
+++ resources/views/show/reportdetail.blade.php
@@ -3,10 +3,23 @@
 
 @section('content')
 @php( $users = \App\User::where('id', $data[0]->user_id)->get())
-<h1>{{$data[0]->id}}</h1>
-<h1>{{$data[0]->title}}</h1>
-<h1>{{$users[0]->users_information[0]->name}}</h1>
-<h1>{{$data[0]->content}}</h1>
-<h1>{{$data[0]->start}}</h1>
-<h1>{{$data[0]->end}}</h1>
-@endsection
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card text-center p-5 shadow-lg p-3 mb-5 bg-white rounded">
+                <div class="card-header">
+                    Daily Report
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">{{$data[0]->title}}</h5>
+                    <p class="">{{$data[0]->content}}</p>
+                    <p class="card-text"> by {{$users[0]->users_information[0]->name}}</p>
+                    <a href="/calendar" class="btn btn-primary">Go Back</a>
+                </div>
+                <div class="card-footer text-muted">
+                    {{$data[0]->start}} ~ {{$data[0]->end}}
+                </div>
+            </div>
+        </div>
+    </div>
+    @endsection
